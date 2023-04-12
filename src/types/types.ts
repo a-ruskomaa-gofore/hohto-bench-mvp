9d8c563db95c4534e295e8d746d9f04a4be7b884
Aleksi Ruskomaa <aleksi.ruskomaa@gofore.com>
2023-04-12 18:15:26 +0300

    Create Employee type, add mock data

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,1 +1,12 @@
-export default {};
+export interface Employee {
+    name: string;
+    imageUrl?: string;
+    experienceYears: number;
+    freeCapacity: number;
+    keySkills: Array<string>;
+    status: "free" | "offered" | "2ndRound"
+}
+
+export interface EmployeeService {
+    fetchEmployeeData: () => Promise<Array<Employee>>
+}
