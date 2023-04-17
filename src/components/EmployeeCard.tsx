e3b651794dbcc586add9d7bfea3f4359f87c068c
Aleksi Ruskomaa <aleksi.ruskomaa@gofore.com>
2023-04-17 12:29:16 +0300

    Layout fixes

--- src/components/EmployeeCard.tsx
+++ src/components/EmployeeCard.tsx
@@ -20,20 +20,20 @@ const EmployeeCard = ({ employee }: CardProps) => {
       <CardContent>
         <Grid container spacing={1}>
           {/* Name */}
-          <Grid item xs={10}>
+          <Grid item xs>
             <Typography variant="subtitle1">{employee.name}</Typography>
           </Grid>
-          <Grid item xs={2}>
+          <Grid item xs={2} minWidth='4rem'>
             <Chip
               label={employee.freeCapacity + '%'}
-              sx={{ margin: '1px' }}
+              sx={{ margin: '1px', minWidth: 'fit-content' }}
             ></Chip>
           </Grid>
           <Grid item xs={12}>
             <Divider />
           </Grid>
           {/* Avatar + experience */}
-          <Grid item xs={3}>
+          <Grid item xs={3} sx={{ minWidth: 'fit-content' }}>
             <Grid container direction="column" alignItems="center">
               <Grid item xs={4}>
                 <Box marginBottom={1}>
@@ -49,7 +49,7 @@ const EmployeeCard = ({ employee }: CardProps) => {
             </Grid>
           </Grid>
           {/* Key skills */}
-          <Grid item xs={9}>
+          <Grid item xs>
             {employee.keySkills.slice(0, 6).map((skill) => (
               <Chip
                 label={skill}
@@ -61,7 +61,7 @@ const EmployeeCard = ({ employee }: CardProps) => {
                   fontWeight: 'bold',
                   borderRadius: 2,
                 }}
-                key={employee.id}
+                key={skill}
               ></Chip>
             ))}
           </Grid>
